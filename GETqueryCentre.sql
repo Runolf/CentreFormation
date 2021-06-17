ccc29640feb9b53a1a1e6312881f5ef2f08759be
Alex <alexandreliskiewicz@hotmail.com>
2021-06-17 22:47:45 +0200

    init comit

---
+++ GETqueryCentre.sql
@@ -0,0 +1,28 @@
+use CentreFormation;
+go
+
+select * from dbo.Formateurs;
+
+select * from dbo.Formations;
+
+select * from dbo.FormationsPrerequises;
+
+select * from dbo.Gestionnaires;
+
+select * from dbo.Inscriptions;
+
+select * from dbo.Locaux;
+
+select * from dbo.Secretaires;
+
+select * from dbo.Sessions;
+
+select * from dbo.Stagiaires;
+
+select * from dbo.Statuts;
+
+select * from dbo.Types;
+
+--insert into dbo.Stagiaires (Nom, Prenom, Email, Pass) 
+--values ('toto', 'toto', 'toto@hotmail.com', '$2a$12$WwkwL0/yOVXajo42TpwWKOrwVYppL081Xnl3srM.8RzUEzHCfyzq.');
+
